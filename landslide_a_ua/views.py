984060489ef628659abeeeac6a2ad466c89d48e3
youketero <youketeroamano22@gmail.com>
2019-11-06 22:09:12 +0200

    build

--- landslide_a_ua/views.py
+++ landslide_a_ua/views.py
@@ -1,3 +1,6 @@
+import http
+
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 
 from landslide_a.forms import form_user
@@ -44,13 +47,12 @@ def contact_ua(request):
             last_name = request.POST.get("last_name")
             mail = request.POST.get("mail")
             phone = request.POST.get("phone")
-            f = form_user1(name=name, last_name=last_name, mail=mail, phone=phone)
+            f = form_user1(name=name,last_name=last_name,mail=mail,phone=phone)
             f.save()
-            return render(request, "contact.html", locals())
+            return render(request, "contact_ua.html", locals())
     else:
         form = form_user()
-        return render(request, "contact.html", locals())
-
+        return render(request, "contact_ua.html", locals())
 
 def case_studies_ua(request):
     partner = About.objects.all()[1:]
